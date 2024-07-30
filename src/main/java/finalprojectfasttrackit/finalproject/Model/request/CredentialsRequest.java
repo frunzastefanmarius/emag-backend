07d954ea19450cd18958eeee3fcaf3ed8bd4acbd
stefanmariusfrunza <stefan.frunza97@gmail.com>
2024-07-30 13:39:21 +0300

    solved issue

--- src/main/java/finalprojectfasttrackit/finalproject/Model/request/CredentialsRequest.java
+++ src/main/java/finalprojectfasttrackit/finalproject/Model/request/CredentialsRequest.java
@@ -1,31 +1,31 @@
-//package finalprojectfasttrackit.finalproject.Model.request;
-//
-//public class CredentialsRequest {
-//
-//    private String username;
-//
-//    private String password;
-//
-//    public CredentialsRequest() {}
-//
-//    public CredentialsRequest(String username, String password) {
-//        this.username = username;
-//        this.password = password;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//}
+package finalprojectfasttrackit.finalproject.Model.request;
+
+public class CredentialsRequest {
+
+    private String username;
+
+    private String password;
+
+    public CredentialsRequest() {}
+
+    public CredentialsRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

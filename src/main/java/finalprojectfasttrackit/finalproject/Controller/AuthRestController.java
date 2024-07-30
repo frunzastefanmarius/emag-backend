7ac047ea80ec2875c9af4bb207237cf91e6cb1a7
stefanmariusfrunza <stefan.frunza97@gmail.com>
2024-07-30 13:37:48 +0300

    acum merge

--- src/main/java/finalprojectfasttrackit/finalproject/Controller/AuthRestController.java
+++ src/main/java/finalprojectfasttrackit/finalproject/Controller/AuthRestController.java
@@ -1,39 +1,39 @@
-//package finalprojectfasttrackit.finalproject.Controller;
-//
-//import finalprojectfasttrackit.finalproject.Model.Users;
-//import finalprojectfasttrackit.finalproject.Model.request.CredentialsRequest;
-//import finalprojectfasttrackit.finalproject.Service.UsersService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@CrossOrigin
-//@RestController
-//@RequestMapping("/auth")
-//public class AuthRestController {
-//
-//    @Autowired
-//    private UsersService myUsersService;
-//
-//    @PostMapping("/signup")
-//    public ResponseEntity<Users> signUp(@RequestBody CredentialsRequest myUser){
-//        try {
-//            Users newUser = new Users(myUser.getUsername(), myUser.getPassword());
-//            newUser.setIsBuyer(true);
-//
-//            return ResponseEntity.ok(myUsersService.createUser(newUser));
-//        } catch (Exception e) {
-//            return ResponseEntity.badRequest().build();
-//        }
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<Users> login(@RequestBody CredentialsRequest myUser){
-//        try {
-//            return ResponseEntity.ok(myUsersService.authenticateUser(myUser.getUsername(), myUser.getPassword()));
-//        } catch (Exception e) {
-//            return ResponseEntity.badRequest().build();
-//        }
-//    }
-//
-//}
+package finalprojectfasttrackit.finalproject.Controller;
+
+import finalprojectfasttrackit.finalproject.Model.Users;
+import finalprojectfasttrackit.finalproject.Model.request.CredentialsRequest;
+import finalprojectfasttrackit.finalproject.Service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/auth")
+public class AuthRestController {
+
+    @Autowired
+    private UsersService myUsersService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Users> signUp(@RequestBody CredentialsRequest myUser){
+        try {
+            Users newUser = new Users(myUser.getUsername(), myUser.getPassword());
+            newUser.setIsBuyer(true);
+
+            return ResponseEntity.ok(myUsersService.createUser(newUser));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Users> login(@RequestBody CredentialsRequest myUser){
+        try {
+            return ResponseEntity.ok(myUsersService.authenticateUser(myUser.getUsername(), myUser.getPassword()));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+}
